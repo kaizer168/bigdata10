c4b4b3919aab4b7b4f2e36669a2e19cba49fe428
kaizer168 <96624836+kaizer168@users.noreply.github.com>
2022-05-22 15:32:54 +0000

    Update CompactTableCommand.scala

--- CompactTableCommand.scala
+++ CompactTableCommand.scala
@@ -3,9 +3,20 @@ package org.apache.spark.sql.execution.command
 import org.apache.spark.sql.{Row, SparkSession}
 import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeReference}
 import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.functions.rand
 
-case class CompactTableCommand() extends LeafRunnableCommand {
+case class CompactTableCommand(
+  table: tableIdentifier,
+  partition: Option[partitionSpec]) extends LeafRunnableCommand {
   override def run(sparkSession: SparkSession): Seq[Row] = {
+//To-do
+    val catalog = sparkSession.sessionState.catalog
+    val targetTable = catalog.getTableMetadata(table)
+    df.repartition(fileNum, $"key", rand)
+      .write
+      .option("header", "true")
+      .partitionBy("key")
+      .csv("/path/partitioned")
 //To-do
     Seq.empty[Row]
   }
